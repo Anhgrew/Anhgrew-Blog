c54908bf43ee19c2041084305a11637690419494
Nguyen Tien Anh <anhtiennguyen-kms@prxcision.com>
2023-05-15 17:45:23 +0700

    Update Gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :notes
+  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :articles
   # Defines the root path route ("/")
   # root "articles#index"
+
   root "pages#home"
   get "about", to: "pages#about"
+
 end
